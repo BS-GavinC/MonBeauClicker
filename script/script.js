382d526954c86e1a70b01e975c8bb3c77d6b8ec6
VZGLY <chaineux.gavin@gmail.com>
2022-10-10 15:43:59 +0200

    premierCommit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+
+let nbr = 0
+
+document.getElementById('monNbr').innerHTML = nbr.toString()
+
+
+function increment(){
+    nbr++
+    document.getElementById('monNbr').innerHTML = nbr.toString()
+}
+
+
+
+
+
+
+
